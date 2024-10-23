4c675d2cef1084d25e8a6410291c7e94c7e36889
benoit <benoit.zenone@gmail.com>
2024-10-23 14:45:51 +0200

    add pokemonList

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
     <>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbasaur"
-        />
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt="Bulbasaur" />
+        ) : (
+          <p>???</p>
+        )}
       </figure>
-      <figcaption>Bulbasaur</figcaption>
+      <figcaption>{pokemon.name}</figcaption>
     </>
   );
 }
