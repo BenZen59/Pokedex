af2f1de23f2278c3df2069cd78a46f9cd6546dfa
benoit <benoit.zenone@gmail.com>
2024-10-23 14:25:09 +0200

    add bulbizarre on new branch

---
+++ src/components/MyTitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyTitle() {
+  return (
+    <h1>Hello React</h1>
+  )
+}
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PokemonCard() {
+  return (
+    <>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+      </figure>
+      <figcaption>Bulbasaur</figcaption>
+    </>
+  );
+}
